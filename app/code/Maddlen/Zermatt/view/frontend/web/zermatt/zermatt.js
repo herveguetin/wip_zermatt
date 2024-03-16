1aea057dd2af0afe63c29a7e7a9b0f9ab81d2c49
Hervé Guétin <herve.guetin@gmail.com>
2024-03-16 11:26:02 +0100

    Better init
    Better init

--- app/code/Maddlen/Zermatt/view/frontend/web/zermatt/zermatt.js
+++ app/code/Maddlen/Zermatt/view/frontend/web/zermatt/zermatt.js
@@ -1,14 +1,4 @@
 import Config from './zermatt.config.js'
-import Alpine from 'alpinejs'
 import Zermatt from 'zermatt-core'
 
-window.Alpine = Alpine
-
-Zermatt.Module.loadAll(Config)
-    .then(modules => {
-        modules.map(module => Alpine.data(module.name, () => module.default))
-        Alpine.start()
-    }).catch(error => {
-        console.error('An error occurred:', error)
-    }
-)
+Zermatt.init(Config)
