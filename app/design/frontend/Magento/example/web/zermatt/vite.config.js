15864f2b5ed53e74de958512888e38df6e5ec9da
Hervé Guétin <herve.guetin@gmail.com>
2024-03-27 21:59:20 +0100

    Prettier

--- app/design/frontend/Magento/example/web/zermatt/vite.config.js
+++ app/design/frontend/Magento/example/web/zermatt/vite.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/namespace
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        manifest: true
-    }
-})
+  build: {
+    manifest: true,
+  },
+});
