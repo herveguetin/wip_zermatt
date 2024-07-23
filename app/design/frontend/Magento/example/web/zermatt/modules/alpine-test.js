1a1f8ab81522ac56d759de603793d00375657f7b
Hervé Guétin <herve.guetin@gmail.com>
2024-07-23 16:36:46 +0200

    devs

--- app/design/frontend/Magento/example/web/zermatt/modules/alpine-test.js
+++
@@ -1,6 +0,0 @@
-export default {
-    name: 'John Doe',
-    greet() {
-        return $t('Welcome to Zermatt %1', this.name)
-    }
-}
--- app/design/frontend/Magento/example/web/zermatt/zermatt.js
+++ app/design/frontend/Magento/example/web/zermatt/zermatt.js
@@ -1,11 +1,4 @@
 import Config from './zermatt-lock.json'
 import Zermatt from 'zermatt-core'
 
-// Import new AlpineJS plugins
-import anchor from '@alpinejs/anchor'
-import resize from '@alpinejs/resize'
-
-// Use AlpineJS plugins on Zermatt init.
-Zermatt.Event.on('zermatt:init', () => Alpine.plugin(anchor, resize))
-
 Zermatt.init(Config)
